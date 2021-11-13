187d7daf85d2f9f1d5ee060ffd89e6d063b446f3
Teng Man <mantou0520@users.noreply.github.com>
2021-11-13 18:29:46 +0800

    Update scanPileForPeaks.m

--- scanPileForPeaks.m
+++ scanPileForPeaks.m
@@ -1,17 +1,17 @@
-function peak_pos = scanPileForPeaks(pile, pile_width)
+function peak_pos = scanPileForPeaks(pile, no_of_voronoi)
 %scanPileForPeaks - Find all peaks in a sandpile
 %
 % Syntax:  peak_pos = scanPileForPeaks(pile)
 %
 % Inputs:
-%    pile - Matrix of shape (pile width, pile width) with integer values 
-%    from 0 to 4
+%    pile - information of the voronoi sand pile
+%    no_of_voronoi - number of voronoi elements
 %
 % Outputs:
 %    peak_pos - Vector containing positions of all peaks
 %
 % Example:
-%    peak_pos = scanPileForPeaks([0 1 3; 4 1 2; 2 1 0]);
+%    peak_pos = scanPileForPeaks([0 1 3; 4 1 2; 2 1 0], 9);
 %
 % Other m-files required: none
 % Subfunctions: none
@@ -25,7 +25,7 @@ function peak_pos = scanPileForPeaks(pile, pile_width)
 
 %------------- BEGIN CODE --------------
 peak_pos = [];
-for ik = 1:pile_width
+for ik = 1:no_of_voronoi
     if pile(ik,3)>=pile(ik,4)+1
         peak_pos = [peak_pos, ik];
     end
