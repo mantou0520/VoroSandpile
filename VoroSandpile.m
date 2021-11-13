e123cbd478a56bf69a901ef23a2a5aad448bd6ba
Teng Man <mantou0520@users.noreply.github.com>
2021-11-13 18:20:30 +0800

    Update VoroSandpile.m

--- VoroSandpile.m
+++ VoroSandpile.m
@@ -1,11 +1,16 @@
-function [avalanche_output, pile, avalanche_store] = VoroSandpile(pile_width, no_of_grains, ...
-    draw_speed)
+function [avalanche_output, pile, avalanche_store] = VoroSandpile(pile_width, no_of_voronoi, no_of_grains, ...
+    boundary_gap, draw_speed)
 
+% main function for simulating the sand pile model, where we construct the
+% initial configuration, size of the system, and start to add sand grains
+% to the system
+% author: Teng Man, manteng0520@outlook.com
+% Reference: https://github.com/flrs/visual_sandpile
 
 
 %------------- BEGIN CODE --------------
 %% check inputs
-assert(nargin >= 3, 'The function %s needs at least 3 inputs.', mfilename);
+assert(nargin >= 4, 'The function %s needs at least 4 inputs.', mfilename);
 
 check_vars = {pile_width, 'pile_width';
     no_of_grains, 'no_of_grains';
@@ -24,14 +29,14 @@ for ct = 1:2
 end
 
 %% initilization
-pile = zeros(pile_width,4);
-pile_store = zeros([pile_width, 4, 1]);
+pile = zeros(no_of_voronoi,4);
+pile_store = zeros([no_of_voronoi, 4, 1]);
 pile_store_add = 0;
 avalanche_plt = 0;  
 avalanche_store = [];
 
-xpos = pile_width.*rand([1 pile_width]);
-ypos = pile_width.*rand([1 pile_width]);
+xpos = pile_width.*rand([1 no_of_voronoi]);
+ypos = pile_width.*rand([1 no_of_voronoi]);
 
 h = voronoi(xpos,ypos);
 % get the position vector of all the particles
@@ -41,37 +46,47 @@ Pos = Pos';
 [Vert Edge] = voronoin(Pos);
 % get the neighbor matrix
 vn = voronoi_neighbors(Pos);
-particles = SandData.empty(pile_width,0);
-for i = 1:pile_width
+particles = SandData.empty(no_of_voronoi,0);
+EdgeParticle = [];
+CtEdge = 1;
+for i = 1:no_of_voronoi
     particles(i).Pos = Pos(i,:);
     particles(i).ID = i;
     count = 0;
-    for j = 1:pile_width
+    for j = 1:no_of_voronoi
         if j ~= i && vn(i,j)==1
             count = count+1;
             particles(i).NbrID(count)=j;
             particles(i).NbrNum = count;
         end
     end
-    particles(i).Value = randi([1, max(1,ceil(0.8*particles(i).NbrNum))]);
+    
+%     particles(i).Value = randi([1, max(1,ceil(0.5*particles(i).NbrNum))]);
+    particles(i).Value = 0;
     pile(i,1) = xpos(i);
     pile(i,2) = ypos(i);
     pile(i,3) = particles(i).Value;
     pile(i,4) = particles(i).NbrNum;
+    
+    if pile(i,1)<=boundary_gap || pile(i,1)>=pile_width-boundary_gap || ...
+            pile(i,2)<=boundary_gap || pile(i,2)>=pile_width-boundary_gap
+        EdgeParticle(CtEdge) = i;
+        CtEdge = CtEdge + 1;
+    end
 end
 
 avalanche_output = [];
 
 % initialize plots
 [pointer_patch, pile_img, avalanche_ct_plot, avalanche_desc_text] = ...
-    VoroSetupPlots(pile_width, draw_speed, pile, Vert, Edge);
+    VoroSetupPlots(pile_width, no_of_voronoi, draw_speed, pile, Vert, Edge);
 
 %% run model
 % generate pile
 for ct = 1:no_of_grains
     % add grain to pile
     fprintf('Adding grain %.0f of %.0f...\n', ct, no_of_grains);
-    add2id = randi([1, pile_width]);
+    add2id = randi([1, no_of_voronoi]);
     
     pile(add2id,3) = pile(add2id, 3)+1;
     particles(add2id).Value = particles(add2id).Value + 1;
@@ -80,17 +95,17 @@ for ct = 1:no_of_grains
     avalanche_size = 0;
     
     % resolve peak
-    peaks = scanPileForPeaks(pile, pile_width);
+    peaks = scanPileForPeaks(pile, no_of_voronoi);
     intermediate_piles = [];
     while numel(peaks) ~= 0
-        [pile, particles, intermediate_piles] = resolveVoroPeaks(pile, particles, peaks, pile_width);
+        [pile, particles, intermediate_piles] = resolveVoroPeaks(pile, particles, peaks, no_of_voronoi);
         if ~isempty(intermediate_piles)
             if draw_speed
                 pile_store = cat(3, pile_store, intermediate_piles);
             end
             avalanche_size = avalanche_size + size(intermediate_piles, 3);
         end
-        peaks = scanPileForPeaks(pile, pile_width);
+        peaks = scanPileForPeaks(pile, no_of_voronoi);
     end
     
     % update avalanche counter
@@ -106,6 +121,12 @@ for ct = 1:no_of_grains
         avalanche_store = [avalanche_store, avalanche_size];
     end
     
+    % erase all the sand grains in the sinking region
+    if mod(ct,500)==0
+        for iEdge = 1:length(EdgeParticle)
+            pile(EdgeParticle(iEdge), 3) = 0;
+        end
+    end
     % call plot function
 %     plotVoroPile(pile_store, pile_store_add, pile_img, pointer_patch,...
 %         ct, avalanche_plt, avalanche_ct_plot, avalanche_desc_text,...
@@ -113,24 +134,5 @@ for ct = 1:no_of_grains
     
     
 end
-
-%% final plot
-figure
-h = histogram(avalanche_store,'BinWidth',1)
-set(gca,'XScale','log')
-set(gca,'YScale','log')
-xlim([1 100])
-
-histoEdge = h.BinEdges;
-histoData = h.Values;
-figure
-plot(0.5*(histoEdge(1:end-1)+histoEdge(2:end)), histoData, 'o','MarkerEdgeColor','k',...
-    'MarkerFaceColor','g','MarkerSize',8)
-set(gca,'XScale','log')
-set(gca,'YScale','log')
-xlabel('Avalanche Size')
-ylabel('No. of observed avalanches')
-set(gca,'FontName','Times New Roman')
-% set(gca,'FontName','Nimbus Roman') % for Ubuntu system
 
 
